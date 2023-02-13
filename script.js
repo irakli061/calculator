c3c6452cb03b8a5cecdf18e8cc43f49b3d8a50ee
irakli061 <irakliimnaishvili13@gmail.com>
2023-02-13 13:37:44 +0400

    Calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+function plus() {
+  let number1 = document.getElementById("Number1");
+  let number2 = document.getElementById("Number2");
+  let answer = document.getElementById("answer");
+  answer.innerHTML = Number(number1.value) + Number(number2.value);
+}
+
+function minus() {
+  let number1 = document.getElementById("Number1");
+  let number2 = document.getElementById("Number2");
+  let answer = document.getElementById("answer");
+  answer.innerHTML = number1.value - number2.value;
+}
+
+function multiply() {
+  let number1 = document.getElementById("Number1");
+  let number2 = document.getElementById("Number2");
+  let answer = document.getElementById("answer");
+  answer.innerHTML = number1.value * number2.value;
+}
+
+function devide() {
+  let number1 = document.getElementById("Number1");
+  let number2 = document.getElementById("Number2");
+  let answer = document.getElementById("answer");
+  answer.innerHTML = number1.value / number2.value;
+}
+
+function DELL() {
+  let number1 = document.getElementById("Number1");
+  let number2 = document.getElementById("Number2");
+  number1.value = "";
+  number2.value = "";
+}
