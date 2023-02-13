8b08070fef76a3af1fbd540a228ec2f8ac7eae75
irakli061 <irakliimnaishvili13@gmail.com>
2023-02-13 13:42:41 +0400

    little AC changes

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ function devide() {
 function DELL() {
   let number1 = document.getElementById("Number1");
   let number2 = document.getElementById("Number2");
+  let answer = document.getElementById("answer");
   number1.value = "";
   number2.value = "";
+  answer.value = "";
 }
